4d3a531a797c927bc2fd69c94a3aea4827417200
Martin Hermansen <martin.hermansen@hotmail.com>
2021-03-28 18:55:38 +0200

    First commit

---
+++ ds_ros2_drone/px4_offboard_control.py
@@ -0,0 +1,129 @@
+from px4_msgs.msg import OffboardControlMode
+from px4_msgs.msg import TrajectorySetpoint
+from px4_msgs.msg import Timesync
+from px4_msgs.msg import VehicleCommand
+from px4_msgs.msg import VehicleControlMode 
+
+import rclpy
+from rclpy.node import Node
+
+
+class PX4OffboardControl(Node):
+	def __init__(self):
+		super().__init__("px4_offboard_control")
+
+		# Creating publishers
+		self.offboard_control_mode_publisher_ = self.create_publisher(OffboardControlMode, "OffboardControlMode_PubSubTopic", 10)
+		self.trajectory_setpoint_publisher_ = self.create_publisher(TrajectorySetpoint, "TrajectorySetpoint_PubSubTopic", 10)
+		self.vehicle_command_publisher_ = self.create_publisher(VehicleCommand, "VehicleCommand_PubSubTopic", 10)
+		
+		# Creating subscribers
+		self.timesync_sub_ = self.create_subscription(Timesync, "Timesync_PubSubTopic", self.timesync, 10)	
+		self.subscription = self.create_subscription(TrajectorySetpoint, "use_drone_setpoint", self.fetch_trajectory_setpoint, 10)
+		
+		# Setting member and locale variables
+		self.x = 0.0
+		self.y = 0.0
+		self.z = 0.0
+		self.yaw = 0.0
+		self.timestamp = 0
+		timer_period = 0.1
+	
+		
+		# Getting ready to fly
+		self.arm() # Arming
+		self.publish_offboard_control_mode() # Control modes..
+		self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_DO_SET_MODE, 1, 6) # Control modes..
+		
+		# Running
+		self.timer = self.create_timer(timer_period, self.timer_callback)
+		
+	# Fetch setpoints	
+	def fetch_trajectory_setpoint(self, use_msg):
+		self.x = use_msg.x 
+		self.y = use_msg.y
+		self.z = use_msg.z
+		self.yaw = use_msg.yaw
+		
+	# Spinning function	
+	def timer_callback(self):
+		self.publish_offboard_control_mode()
+		self.publish_trajectory_setpoint
+	
+	# Fetch timestamp	
+	def timesync(self, px4_time):
+		self.timestamp = px4_time.timestamp
+	
+        
+	# Send a command to Arm the vehicle
+	def arm(self):
+		self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0, 0.0)
+		self.get_logger().info("Arm command send")
+        
+	# Send a command to Disarm the vehicle
+	def disarm(self):
+		self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0, 0.0)
+		self.get_logger().info("Disarm command send")
+        
+	# Publish the offboard control mode.
+	def publish_offboard_control_mode(self):
+		msg = OffboardControlMode()
+
+		msg.timestamp = self.timestamp
+		msg.position = True
+		msg.velocity = False
+		msg.acceleration = False
+		msg.attitude = False
+		msg.body_rate = False
+
+		self.offboard_control_mode_publisher_.publish(msg)
+		
+	# Publish trajectory setpoint
+	def publish_trajectory_setpoint(self):
+		msg = TrajectorySetpoint()
+
+		msg.timestamp = self.timestamp
+		msg.x = self.x
+		msg.y = self.y
+		msg.z = self.z
+		msg.yaw = self.yaw
+
+		self.trajectory_setpoint_publisher_.publish(msg)
+		
+	# Publish vehicle command
+	def publish_vehicle_command(self, command, param1, param2):
+		msg = VehicleCommand()
+
+		msg.timestamp = self.timestamp
+		msg.param1 = float(param1)
+		msg.param2 = float(param2)
+		msg.command = command
+		msg.target_system = 1
+		msg.target_component = 1
+		msg.source_system = 1
+		msg.source_component = 1
+		msg.from_external = True
+
+		self.vehicle_command_publisher_.publish(msg)
+
+	# Spinning function
+	def timer_callback(self):
+		self.publish_offboard_control_mode()
+		self.publish_trajectory_setpoint()
+
+
+
+def main(args=None):
+	rclpy.init(args=args)
+
+	px4_offboard_control = PX4OffboardControl()
+
+	rclpy.spin(px4_offboard_control)
+
+	px4_offboard_control.destroy_node()
+
+	rclpy.shutdown()
+
+
+if __name__ == "__main__":
+	main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'ds_ros2_drone'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rostest1',
+    maintainer_email='martin.hermansen@hotmail.com',
+    description='Python package on for PX4 to ROS2 communication.',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        	'px4_offboard_control = ds_ros2_drone.px4_offboard_control:main',
+        ],
+    },
+)
